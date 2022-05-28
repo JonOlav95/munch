f50b88c58896727276db6765a729c78b6aba1d83
jo95holland <jo95holland@gmail.com>
2022-05-28 14:47:03 +0200

    Loss parameteres applied

--- train.py
+++ train.py
@@ -21,8 +21,14 @@ def discriminator_loss(disc_real_output, disc_generated_output):
 
 
 def generator_loss(disc_generated_output, gen_output, target):
-    gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
-    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+    l1_loss = 0
+    gan_loss = 0
+
+    if FLAGS("l1_loss"):
+        l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+    if FLAGS("disc_loss"):
+        gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
 
     total_gen_loss = gan_loss + (FLAGS["l1_lambda"] * l1_loss)
 
