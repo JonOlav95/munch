318f78bf7623ea2b45d0f0b7c43ab38372c66f8f
jo95holland <jo95holland@gmail.com>
2022-06-27 20:09:34 +0200

    Moved hinge

--- loss_func.py
+++ loss_func.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 from config import FLAGS
 
-loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=False)
-
+hinge = tf.keras.losses.Hinge()
 
 def discriminator_loss(disc_real_output, disc_generated_output):
-    hinge = tf.keras.losses.Hinge()
     disc_real_loss = hinge(tf.ones_like(disc_real_output), disc_real_output)
     disc_gen_loss = hinge(tf.zeros_like(disc_generated_output), disc_generated_output)
 
@@ -21,7 +19,6 @@ def generator_loss(disc_generated_output, gen_output, target):
     if FLAGS["l1_loss"]:
         l1_loss = FLAGS["l1_lambda"] * tf.reduce_mean(tf.abs(target - gen_output))
     if FLAGS["disc_loss"]:
-        hinge = tf.keras.losses.Hinge()
         gan_loss = hinge(tf.ones_like(disc_generated_output), disc_generated_output)
 
     total_gen_loss = gan_loss + l1_loss
