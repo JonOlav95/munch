09e5b0402fd59409f8ce82b6e3950f3ed23003bc
jo95holland <jo95holland@gmail.com>
2022-06-28 02:34:58 +0200

    Fixed plotting

--- plotter.py
+++ plotter.py
@@ -24,7 +24,12 @@ def inner_plot(batch, discriminator, generator):
     masks = reiterate_mask(mask)
 
     generated_image = generator([masked_batch, masks], training=False)
-    generated_image = generated_image[0, ...]
+
+    stage1_gen = generated_image[0]
+    stage2_gen = generated_image[1]
+
+    stage1_gen = stage1_gen[0, ...]
+    stage2_gen = stage2_gen[0, ...]
 
     x = masked_batch[0, ...]
     y = batch[0, ...]
@@ -32,7 +37,7 @@ def inner_plot(batch, discriminator, generator):
     #disc_gen_result = discriminator([generated_image], training=False)
     #disc_gen_result = round(tf.math.reduce_mean(disc_gen_result).numpy(), 2)
 
-    images = [x, mask, generated_image, y]
+    images = [x, stage1_gen, stage2_gen, y]
 
     fig = plt.figure()
     for i in range(len(images)):
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 from data_handler import load_data
 from discriminator import discriminator
-from generator import st_generator
+from generator import st_generator, gated_generator
 from config import FLAGS, generator_optimizer, discriminator_optimizer
 from plotter import plot_all
 
 
 def test():
-    model = st_generator(FLAGS.get("img_size"))
+    model = gated_generator(FLAGS.get("img_size"))
     disc = discriminator(FLAGS.get("img_size"))
     ds = load_data(FLAGS["testing_samples"])
 
