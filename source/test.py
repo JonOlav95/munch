55e40a6ddc3733109887000e9af28980a078e174
jo95holland <jo95holland@gmail.com>
2022-07-03 10:23:58 +0200

    Structure

--- source/test.py
+++ source/test.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from data_handler import load_data
 from patch_discriminator import discriminator
 from generator_gated import gated_generator
-from config import FLAGS, generator_optimizer, discriminator_optimizer
+from config import FLAGS
 from plotter import plot_all
 
 
@@ -11,6 +11,9 @@ def test():
     model = gated_generator(FLAGS.get("img_size"))
     disc = discriminator(FLAGS.get("img_size"))
     ds = load_data(FLAGS["testing_samples"])
+
+    generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
     checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                      discriminator_optimizer=discriminator_optimizer,
